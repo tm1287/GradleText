af40dfadeb8ee839f151d1c8ff58dfe084653f0c
Tejas Maraliga <tejas.maraliga@gmail.com>
2020-03-23 14:13:54 -0700

    Create plugin extension with Twilio config values

---
+++ src/main/kotlin/com/tejasm/gradle/GradleTextPluginExtension.kt
@@ -0,0 +1,7 @@
+package com.tejasm.gradle
+
+open class GradleTextPluginExtension (
+    var targetPhoneNumber: String? = "",
+    var sourcePhoneNumber: String? = "",
+    var messageBody: String? = ""
+)
